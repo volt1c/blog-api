e62799ae31e14fab0d4c79e2ef89ad04b6a15e13
Kamil Pawlaczyk <prgmr.kamil.pawlaczyk@gmail.com>
2022-01-05 17:32:02 +0100

    feat: add `post` model

---
+++ src/models/post.ts
@@ -0,0 +1,29 @@
+import { Schema, Document, model } from 'mongoose'
+import { IUser } from './user'
+import { IComment } from './comment'
+
+const postSchema = new Schema({
+  id: String,
+  title: String,
+  content: String,
+  sender: { type: Schema.Types.ObjectId, ref: 'User' },
+  comments: [{ type: Schema.Types.ObjectId, ref: 'Comment' }],
+  cratedAt: Date,
+})
+
+interface IPost extends Document {
+  id: string
+  title: string
+  content: string
+  sender: IUser
+  comments: IComment[]
+  createdAt: Date
+}
+interface ICreatePost {
+  title: IPost['title']
+  content: IPost['content']
+  sender: IPost['sender']
+}
+
+const Post = model<IPost>('Post', postSchema)
+export { Post, IPost, ICreatePost }
