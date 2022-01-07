ec9904d6aed5940f17cd38bee7c2d0601d2f48aa
Kamil Pawlaczyk <prgmr.kamil.pawlaczyk@gmail.com>
2022-01-07 20:24:49 +0100

    feat: use routes `auth/`, `users/`

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,9 +1,14 @@
 import { Request, Response, Router } from 'express'
+import userRoutes from './users'
+import authRoutes from './auth'
 
 const router = Router()
 
+router.use('/users', userRoutes)
+router.use('/auth', authRoutes)
+
 router.get('/', async (req: Request, res: Response) => {
-  res.end('Api works!')
+  res.status(200).end('Api works!')
 })
 
 export default router
