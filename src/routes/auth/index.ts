9cd79e91eb4390594aa29fe0ec634a43a51b7253
Kamil Pawlaczyk <prgmr.kamil.pawlaczyk@gmail.com>
2022-01-10 10:04:29 +0100

    feat: add login route

--- src/routes/auth/index.ts
+++ src/routes/auth/index.ts
@@ -1,8 +1,10 @@
 import { Router } from 'express'
 import registerRoute from './register'
+import loginRoute from './login'
 
 const router = Router()
 
 router.use(registerRoute)
+router.use(loginRoute)
 
 export default router
---
+++ src/routes/auth/login.ts
@@ -0,0 +1,15 @@
+import { Router } from 'express'
+import passport from 'passport'
+import AuthController from '../../controllers/auth'
+import AuthValidator from '../../validators/auth'
+
+const router = Router()
+
+router.post(
+  '/login',
+  AuthValidator.loginValidator,
+  passport.authenticate('local', { session: false }),
+  AuthController.login
+)
+
+export default router
