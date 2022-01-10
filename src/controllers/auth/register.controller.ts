f94bd7bbf9056410feab5f9dfe5b57f6ddeb1b29
Kamil Pawlaczyk <prgmr.kamil.pawlaczyk@gmail.com>
2022-01-10 09:46:40 +0100

    feat: use `User.register` to register user

--- src/controllers/auth/register.controller.ts
+++ src/controllers/auth/register.controller.ts
@@ -1,20 +1,16 @@
 import { Request, Response } from 'express'
 import { User } from '../../models/user'
-import bcrypt from 'bcrypt'
 
 export async function register(req: Request, res: Response) {
   const { email, name, pass } = req.body
 
-  const hash = bcrypt.hashSync(pass, 12)
-
-  new User({
+  const user = new User({
     name: name,
-    pass: hash,
     email: email,
     avatar: process.env.AVATAR_URL ?? 'https://i.imgur.com/IFtORGZ.png',
   })
-    .save()
-    .then(() => {
-      res.status(201).end()
-    })
+
+  await User.register(user, pass)
+
+  res.status(201).end()
 }
