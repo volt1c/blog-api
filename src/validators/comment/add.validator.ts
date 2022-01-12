fa9d6cff22a30e160930401ce1d12f098de684c2
Kamil Pawlaczyk <prgmr.kamil.pawlaczyk@gmail.com>
2022-01-12 10:59:37 +0100

    feat: correct add comment validator

--- src/validators/comment/add.validator.ts
+++ src/validators/comment/add.validator.ts
@@ -2,5 +2,5 @@ import { body } from 'express-validator'
 
 export default [
   body('postId').isString().isLength({ min: 24, max: 24 }),
-  body('content').isString().isLength({ min: 50, max: 1000 }),
+  body('content').isString().isLength({ min: 1, max: 300 }),
 ]
