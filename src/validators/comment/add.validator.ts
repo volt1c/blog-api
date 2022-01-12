12cbcda99d93cb245addea5f022551d4b8bcbf47
Kamil Pawlaczyk <prgmr.kamil.pawlaczyk@gmail.com>
2022-01-12 09:47:25 +0100

    fix: id validation

--- src/validators/comment/add.validator.ts
+++ src/validators/comment/add.validator.ts
@@ -1,3 +1,6 @@
 import { body } from 'express-validator'
 
-export default [body('postId').isString(), body('content').isString()]
+export default [
+  body('postId').isString().isLength({ min: 24, max: 24 }),
+  body('content').isString().isLength({ min: 50, max: 1000 }),
+]
--- src/validators/post/id.validator.ts
+++ src/validators/post/id.validator.ts
@@ -1,3 +1,3 @@
 import { param } from 'express-validator'
 
-export default [param('id')]
+export default [param('id').isString().isLength({ min: 24, max: 24 })]
